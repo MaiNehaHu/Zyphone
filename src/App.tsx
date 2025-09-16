93ce9f05e557121561fc9a46e60fe18a8f59e21f
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-09-16 12:58:26 +0530

    NavBar

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./layout/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App;
---
+++ src/layout/NavBar.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const NavBar = () => {
+    const [active, setActive] = useState("");
+
+    return (
+        <div>NavBar</div>
+    )
+}
+
+export default NavBar
