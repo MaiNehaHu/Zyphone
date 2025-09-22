2ed8ede1c9ece12c336ba1b257c6cff0f4bdac1d
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-09-22 16:23:38 +0530

    bg black

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -89,7 +89,7 @@ const NavBar = () => {
 
                 {/* Hamburger for mobile */}
                 <button
-                    className="lg:hidden text-white text-2xl bg-[#00000020] backdrop-blur-lg rounded-full border-2 border-[#E5F0F336] px-4 py-2.5 cursor-pointer"
+                    className="lg:hidden text-white text-2xl bg-[#000000] backdrop-blur-lg rounded-full border-2 border-[#E5F0F336] px-4 py-2.5 cursor-pointer"
                     onClick={() => setMenuOpen((prev) => !prev)}
                 >
                     {menuOpen ? <FiX /> : <FiMenu />}
