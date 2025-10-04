20523a61756232e0d81cfbfc17a7316d6c308694
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-10-04 20:38:25 +0530

    why choose us res

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -4,14 +4,14 @@ import { GoDotFill } from "react-icons/go"
 
 const WhyChooseUs = () => {
     return (
-        <div className="bg-[#1F1F1F] text-white p-4 rounded-2xl flex flex-row items-center gap-10 relative">
-            <div className="w-2/5 bg-white rounded-2xl h-[550px]"></div>
+        <div className="bg-[#1F1F1F] text-white p-4 sm:rounded-2xl flex md:flex-row flex-col items-center xl:gap-10 gap-6 relative">
+            <div className="lg:w-2/5 md:w-1/2 w-full bg-white rounded-2xl sm:h-[550px] h-[300px]"></div>
 
-            <div className="w-3/5 flex flex-col gap-10 justify-center py-10 max-w-xl mx-auto">
+            <div className="lg:w-3/5 md:w-1/2 w-full flex flex-col lg:gap-10 gap-6 justify-center md:py-10 py-6 md:px-0 sm:px-6 px-0 md:max-w-xl mx-auto">
                 <div className="flex flex-col gap-6 items-start">
                     <XPaddinText>why choose us</XPaddinText>
 
-                    <h2 className="font-['Space_Grotesk'] text-5xl font-medium">
+                    <h2 className="font-['Space_Grotesk'] sm:text-5xl text-4xl font-medium">
                         Top Reasons to Choose Our Gadget Repair
                     </h2>
                 </div>
@@ -20,10 +20,10 @@ const WhyChooseUs = () => {
                     We’re passionate about keeping your gadgets in top shape. Our team of experienced technicians combine technical expertise with exceptional customer service.
                 </p>
 
-                <div className="grid grid-cols-2 gap-4 font-[Manrope]">
+                <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-4 gap-0 font-[Manrope]">
                     {["Same-day Service", "Genuine Parts", "12-Month Warrenty", "Transparent Pricing", "Free Doorstep Pickup & Delivery"].map((tag) => (
-                        <p key={tag} className="flex flex-row items-center gap-2">
-                            <GoDotFill className="text-2xl" />
+                        <p key={tag} className="flex flex-row items-center gap-2 sm:text-base text-sm">
+                            <GoDotFill className="sm:text-2xl text-xl" />
                             {tag}
                         </p>
                     ))}
